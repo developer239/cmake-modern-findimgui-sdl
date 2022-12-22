5cccf23f1dbf4c4fcdb4af5f52e76da15bc778ef
michaljarnot <m.jarnot@yahoo.com>
2022-12-22 01:03:16 +0100

    feat: make imgui somewhat work

--- FindImgui.cmake
+++ FindImgui.cmake
@@ -1,17 +1,23 @@
 set(IMGUI_LIBRARY ${CMAKE_SOURCE_DIR}/externals/imgui)
 # Not that this depends on SDL2
-set(IMGUI_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/externals/imgui ${IMGUI_DIR}/backends ${SDL2_INCLUDE_DIR})
+set(IMGUI_INCLUDE_DIR
+        ${CMAKE_SOURCE_DIR}/externals/imgui
+        ${CMAKE_SOURCE_DIR}/externals/imgui/backends
+        ${SDL2_INCLUDE_DIR})
 
 add_library(Imgui STATIC)
 
 target_sources(Imgui PUBLIC
         ${IMGUI_LIBRARY}/imgui.cpp
+        ${IMGUI_LIBRARY}/imgui.h
         ${IMGUI_LIBRARY}/imgui_demo.cpp
         ${IMGUI_LIBRARY}/imgui_draw.cpp
         ${IMGUI_LIBRARY}/imgui_tables.cpp
         ${IMGUI_LIBRARY}/imgui_widgets.cpp
         ${IMGUI_LIBRARY}/backends/imgui_impl_sdl.cpp
+        ${IMGUI_LIBRARY}/backends/imgui_impl_sdl.h
         ${IMGUI_LIBRARY}/backends/imgui_impl_sdlrenderer.cpp
+        ${IMGUI_LIBRARY}/backends/imgui_impl_sdlrenderer.h
         )
 
 target_include_directories(Imgui PUBLIC ${IMGUI_INCLUDE_DIR})
